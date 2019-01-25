36f62edcb6314288c9f71d3b2a4e7d90a2b5b361
JackLingwood <Jack.Lingwood@Gmail.com>
2019-01-25 11:45:25 -0500

    Completed a kata

---
+++ Kata/Alpha.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Kata
+{
+    public static class Alpha
+    {
+
+        public static string AlphabetPosition(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(char ch in text)
+            {
+                int subtractor = char.IsUpper(ch) ? 64 : 96;
+                if (char.IsLetter(ch))
+                    sb.Append( ((int)ch-subtractor)+" ");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
